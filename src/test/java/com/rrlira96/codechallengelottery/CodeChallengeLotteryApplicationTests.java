64e0d7f0e000b1a5788c96ede2729f80cad54e7d
rrlira96 <renato.lira.14@gmail.com>
2023-03-15 10:57:36 -0300

    Build: Adding initial config

---
+++ src/test/java/com/rrlira96/codechallengelottery/CodeChallengeLotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrlira96.codechallengelottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChallengeLotteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
